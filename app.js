72c51b1b5ba549bd6370fe65b2ca03094f0af2e5
RichardZhangD <zhangda0000001@163.com>
2018-03-15 21:46:59 +0800

    add front end department function

--- app.js
+++ app.js
@@ -19,6 +19,29 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  if (req.method == 'OPTIONS') {
+      /*让options请求快速返回*/
+      res.send(200);
+  }
+  else {
+    /*防止异步造成多次响应，出现错误*/
+    var _send = res.send;
+        var sent = false;
+        res.send = function (data) {
+            if (sent) return;
+            _send.bind(res)(data);
+            sent = true;
+        };
+      next();
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
